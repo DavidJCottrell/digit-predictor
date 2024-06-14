0a1c3bb5155f2cfedae940b7755290900820f159
David Cottrell <davidjcottrell@icloud.com>
2024-06-14 21:43:33 +0100

    update

--- predict.py
+++ predict.py
@@ -8,45 +8,41 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import time
 import random
+from termcolor import colored
 
 model = load_model('num_reader.keras')
 
 def preprocess_image(image_path):
-    img = Image.open(image_path).convert('L') # Convert to grayscale
-    img = img.resize((28, 28)) # Resize to 28x28 (matching MNIST)
+    img = Image.open(image_path).convert('L') 
+    img = img.resize((28, 28)) 
     img_array = np.array(img) / 255.0 # Normalize pixel values
-    img_array = 1 - img_array # Invert colors: MNIST has white digit on black background
-    img_array = np.expand_dims(img_array, axis=0) # Add batch dimension
-    img_array = np.expand_dims(img_array, axis=-1) # Add channel dimension
+    img_array = 1 - img_array # Invert colors as MNIST has white digit on black background
+    img_array = np.expand_dims(img_array, axis=0) 
+    img_array = np.expand_dims(img_array, axis=-1) 
     return img_array
 
 def predict_image(image_path):
     img_array = preprocess_image(image_path)
-    prediction = model.predict(img_array)
-    predicted_digit = np.argmax(prediction)
-    return predicted_digit
+    return model.predict(img_array)
 
-def print_predication(predicted_digit):
+def print_predication(predictions):
     os.system('cls' if os.name == 'nt' else 'clear')
 
-    choice = random.randrange(1, 5)
-    if(choice == 1):
-        print(f"\nNow that's a nice {predicted_digit}!\n")
-    elif(choice == 2):
-        print(f"\nMotherfuckin {predicted_digit}\n")
-    elif(choice == 3):
-        print(f"\nNow would you look at that {predicted_digit}!\n")
-    elif(choice == 4):
-        print(f"\nI've seen a lot of {predicted_digit}'s, but that's the nicest 👍\n")
+    sorted_predictions = np.argsort(-predictions[0])
+
+    for prediction_index in sorted_predictions:
+        print(f"    {colored(prediction_index, 'green', attrs=['bold'])}: {round(predictions[0][prediction_index] * 100, 2)}%")
+        if(prediction_index == np.argmax(predictions)):
+            print("-------------------")
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, _):
         global last_file_change_time
         try:
-            predicted_digit = predict_image(f"./digits/test_number.png")
-            print_predication(predicted_digit)
+            predictions = predict_image(f"./digits/test_number.png")
+            print_predication(predictions)
         except UnidentifiedImageError:
-            print("Error")
+            print("Error reading image file")
 
 if __name__ == "__main__":
     event_handler = MyHandler()
